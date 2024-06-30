1dd8050f0ec014d4207773b1aa88054f0b6ab03e
Ricardo Bianchin <ricardobianchin@gmail.com>
2024-06-30 17:31:26 -0300

    Update MainActivity.kt

--- app/src/main/java/com/example/android_greeting_card/MainActivity.kt
+++ app/src/main/java/com/example/android_greeting_card/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.android_greeting_card
 
 import android.os.Bundle
+import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_greeting_card.ui.theme.AndroidgreetingcardTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +37,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi! I am $name!!",
-        modifier = modifier
-    )
+    Surface(color = Color.Cyan) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidgreetingcardTheme {
-        Greeting("Ricardo Bianchin")
+        Greeting("Ricardo")
     }
 }
